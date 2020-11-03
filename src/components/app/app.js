781ce177daf8b51c19ad03430a7bf12991095be6
iDestar <destarpwnz@gmai.com>
2020-11-03 08:53:29 +0300

    Initial commit

---
+++ src/components/app/app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Header from '../header'
+import ItemList from '../item-list'
+import PersonDetails from '../person-details'
+import RandomPlanet from '../random-planet'
+
+import './app.css';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <RandomPlanet />
+                 
+                 <div className="row mb2">
+                     <div className="col-md-6">
+                       <ItemList />
+                     </div>
+                     <div className="col-md-6">
+                     <PersonDetails />
+                     </div>
+                 </div>
+            </div>
+
+        )
+    }
+}
---
+++ src/components/item-list/item-list.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+import './item-list.css'
+
+export default class ItemList extends Component {
+    render() {
+        return (
+            <ul className="list-group item-list">
+                <li className="list-group-item">
+                Luke Skywalker
+                </li>
+                <li className="list-group-item">
+                Darth Vader   
+                </li>
+                <li className="list-group-item">
+                R2-D2  
+                </li>
+            </ul>
+        )
+    }
+}
